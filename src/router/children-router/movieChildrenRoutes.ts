44e4273e0d13ac880e4dd6a309fcea13b3e4a1d8
chin <149858139+dangkhoa2004@users.noreply.github.com>
2024-11-06 15:04:53 +0700

    removie teset

--- src/router/children-router/movieChildrenRoutes.ts
+++ src/router/children-router/movieChildrenRoutes.ts
@@ -7,11 +7,6 @@ const bookingChildrenRoutes: RouteRecordRaw[] = [
     component: () => import('@/views/movie/index.vue'),
   },
   {
-    path: "test/:id",
-    name: "test-suat-chieu",
-    component: () => import('@/ShowTime.vue'),
-  },
-  {
     path: ":id",
     name: "chi-tiet-phim",
     component: () => import("@/views/movie/component/chooseMovieData_component.vue"),
